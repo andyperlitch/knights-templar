53d04946db24e55034f6af1203e08f8e5ede21e7
Andy Perlitch <andyperlitch@gmail.com>
2013-03-20 11:45:17 -0700

    renamed main.js file

--- knights-templar.js
+++ knights-templar.js
@@ -1,27 +1,29 @@
 var fs = require('fs');
 var path = require('path');
 var handlebars = require('handlebars');
+var _ = require('underscore');
 
-
+// Main make method
 function make(type, path) {
     
-    // check file existence
+    // Check file existence
     if (! fs.existsSync(path) ) throw new Error("Template file could not be found. Given path: `"+path+"`. cwd: `"+process.cwd()+"`.");
     
     // Read the file
     var file_contents = fs.readFileSync(path).toString('utf8');
     
+    // Compile with appropriate library
     switch(type) {
         case "handlebars":
         case "Handlebars":
         case "hbs":
-            handlebars.compile(file_contents);
-            break;
+            return handlebars.compile(file_contents);
         
-        
+        case "_":
+        case "underscore":
+        case "erb":
+            return _.template(file_contents);
     }
 }
-
-exports.hbs = function(path) { return make('hbs',path) }
 
 exports.make = make
