7aeeb47111c2473c5358bf63a0f65fb59541e088
Andy Perlitch <andyperlitch@gmail.com>
2013-03-20 22:49:24 -0700

    switched test harness to mocha

--- test/knight.spec.js
+++ test/knight.spec.js
@@ -1,3 +1,4 @@
+var assert = require('assert');
 describe("A template-generating knight templar", function() {
     
     var knight = require("../knights-templar");
@@ -5,14 +6,14 @@ describe("A template-generating knight templar", function() {
     it("should compile handlebar templates by default", function() {
         var path = __dirname + "/handlebars.html";
         var template = knight.make(path);
-        expect(template({ name:"test" })).toEqual('<p>test</p>');
+        assert.equal(template({ name:"test" }),'<p>test</p>');
     });
     
     it("should compile underscore templates too, with aliases", function() {
         var path = __dirname + "/underscore.html";
         var template = knight.make(path,"_");
         var template2 = knight.make(path,"underscore");
-        expect(template({ name:"test" })).toEqual('<p>test</p>');
+        assert.equal(template({ name:"test" }),'<p>test</p>');
     });
     
     it("should accept common aliases for types", function() {
@@ -22,16 +23,16 @@ describe("A template-generating knight templar", function() {
         var template2 = knight.make(path,"hbs");
         var template3 = knight.make(path,"handlebars");
         var template4 = knight.make(path,"Handlebars");
-        expect(template(data) === template2(data)).toBeTruthy();
-        expect(template(data) === template3(data)).toBeTruthy();
-        expect(template(data) === template4(data)).toBeTruthy();
+        assert.ok(template(data) === template2(data));
+        assert.ok(template(data) === template3(data));
+        assert.ok(template(data) === template4(data));
     });
     
     it("should complain if a file does not exist", function() {
         var badCall = function() {
             knight.make(__dirname+'/notafile')
         }
-        expect(badCall).toThrow();
+        assert.throws(badCall);
     });
     
 })
