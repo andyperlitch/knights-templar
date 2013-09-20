be70658a379a0b09c6c20a72b7566708fdd20ae2
Andy Perlitch <andyperlitch@gmail.com>
2013-09-20 01:08:21 -0700

    added browser support with sync ajax call

--- knights-templar.js
+++ knights-templar.js
@@ -1,26 +1,40 @@
 var fs = require('fs');
 var path = require('path');
-var handlebars = require('handlebars');
+// var handlebars = require('handlebars');
 var _ = require('underscore');
+
+// File reading method
+function getFileContents(path) {
+    // check for browser
+    if (typeof window !== 'undefined') {
+        // make asynchronous XHR
+        var xhr = new XMLHttpRequest();
+        xhr.open('get', (window.KT_BASE_URL || '/') + path, false);
+        var res = xhr.send();
+        return xhr.response;
+    } else {
+        // Check file existence
+        if (! fs.existsSync(path) ) throw new Error("Template file could not be found. Given path: `"+path+"`. cwd: `"+process.cwd()+"`.");
+        return fs.readFileSync(path).toString('utf8');
+    }
+}
+
 
 // Main make method
 function make(path, type) {
     
+    // Read the file
+    var file_contents = getFileContents(path);
+    
     // Default type
     type = type || 'hbs'
     
-    // Check file existence
-    if (! fs.existsSync(path) ) throw new Error("Template file could not be found. Given path: `"+path+"`. cwd: `"+process.cwd()+"`.");
-    
-    // Read the file
-    var file_contents = fs.readFileSync(path).toString('utf8');
-    
     // Compile with appropriate library
     switch(type) {
-        case "handlebars":
-        case "Handlebars":
-        case "hbs":
-            return handlebars.compile(file_contents);
+        // case "handlebars":
+        // case "Handlebars":
+        // case "hbs":
+        //     return handlebars.compile(file_contents);
         
         case "_":
         case "underscore":
